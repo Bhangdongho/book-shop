8ef85712cbaa58a888d4aed0f02d99a1fee5f971
Bhangdongho <bhang97@naver.com>
2024-05-22 01:39:09 +0900

    controller 정리

---
+++ controller/UserController.js
@@ -0,0 +1,73 @@
+const conn = require('../mariadb'); // db 모듈
+const { StatusCodes } = require('http-status-codes'); // status code 모듈
+const jwt = require('jsonwebtoken'); // jwt 모듈
+const dotenv = require('dotenv'); // dotenv 모듈
+dotenv.config();
+
+const join = (req, res) => {
+  const { email, password } = req.body;
+
+  let sql = 'INSERT INTO users (email, password) VALUES (?, ?)';
+  let values = [email, password];
+
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+
+    return res.status(StatusCodes.CREATED).json(results);
+  });
+};
+
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  let sql = 'SELECT * FROM users WHERE email = ?';
+  conn.query(sql, email, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+
+    const loginUser = results[0];
+    if (loginUser && loginUser.password == password) {
+      // 토큰 발행
+      const token = jwt.sign(
+        {
+          email: loginUser.email
+        },
+        process.env.PRIVATE_KEY,
+        {
+          expiresIn: '5m',
+          issuer: 'dongho'
+        }
+      );
+
+      // 토큰 쿠키에 담기
+      res.cookie('token', token, {
+        httpOnly: true
+      });
+      console.log(token);
+
+      return res.status(StatusCodes.OK).json(results);
+    } else {
+      return res.status(StatusCodes.UNAUTHORIZED).end(); // 401 : Unauthorized (비인증)
+    }
+  });
+};
+
+const passwordResetRequest = (req, res) => {
+  res.json('비밀번호 초기화 요청');
+};
+
+const passwordReset = (req, res) => {
+  res.json('비밀번호 초기화');
+};
+
+module.exports = {
+  join,
+  login,
+  passwordResetRequest,
+  passwordReset
+};
