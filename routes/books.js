15910c94527a1907fd711c52be19d5640e97bf16
Bhangdongho <bhang97@naver.com>
2024-05-24 06:57:38 +0900

    bookcontroller

--- routes/books.js
+++ routes/books.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {
-  allBooks,
-  bookDetail,
-  booksByCategory
-} = require('../controller/BookController');
+const { allBooks, bookDetail } = require('../controller/BookController');
 
 router.use(express.json());
 
-router.get('/', booksByCategory); // 카테고리별 도서 목록 조회
-router.get('/', allBooks); // 전체 도서 조회
+router.get('/', allBooks); // (카테고리 별 조회) 전체 도서 조회
 router.get('/:id', bookDetail); // 개별 도시 조회
 
 module.exports = router;
