01f0e62c4ba325a6ac53773d5f44b82c763996db
Bhangdongho <bhang97@naver.com>
2024-05-28 09:13:50 +0900

    5/28 과제

--- controller/CartController.js
+++ controller/CartController.js
@@ -20,13 +20,14 @@ const addToCart = (req, res) => {
 
 // 장바구니 아이템 목록 조회
 const getCartItems = (req, res) => {
-  const { user_id, selected } = req.body;
+  const { user_id, selected } = req.body; // selected = [1, 3]
 
   let sql = `SELECT cartItems.id, book_id, title, summary, quantity, price  
                 FROM cartItems LEFT JOIN books 
                 ON cartItems.book_id = books.id
-                WHERE user_id=? AND id IN (?)`;
-  conn.query(sql, user_id, (err, results) => {
+                WHERE user_id=? AND cartItems.id IN (?)`;
+  let values = [user_id, selected];
+  conn.query(sql, values, (err, results) => {
     if (err) {
       console.log(err);
       return res.status(StatusCodes.BAD_REQUEST).end();
