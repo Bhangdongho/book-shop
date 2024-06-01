21c165077eb598a3ddbbe0248ed56d2b951ca642
Bhangdongho <bhang97@naver.com>
2024-06-01 23:49:50 +0900

    execute

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -14,45 +14,33 @@ const order = async (req, res) => {
   const { items, delivery, totalQuantity, totalPrice, userId, firstBookTitle } =
     req.body;
 
-  let delivery_id;
-  let order_id;
-
+  // delivery 테이블 삽입
   let sql =
     'INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?)';
   let values = [delivery.address, delivery.receiver, delivery.contact];
+  let [results] = await conn.execute(sql, values);
+  let delivery_id = results.insertId;
 
-  let [results] = await conn.query(sql, values);
+  // orders 테이블 삽입
+  sql = `
+    INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id)
+    VALUES (?, ?, ?, ?, ?)`;
+  values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
+  [results] = await conn.execute(sql, values);
+  let order_id = results.insertId;
 
-  console.log(results);
+  // orderedBook 테이블 사입
+  sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`;
 
-  //   sql = `
-  //   INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id)
-  //   VALUES (?, ?, ?, ?, ?)`;
-  //   values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
-  //   conn.query(sql, values, (err, results) => {
-  //     if (err) {
-  //       console.log(err);
-  //       return res.status(StatusCodes.BAD_REQUEST).end();
-  //     }
+  // items.. 배열 : 요소들을 하나씩 꺼내서 (foreach문 돌려서) >
+  values = [];
+  items.forEach((item) => {
+    values.push([order_id, item.book_id, item.quantity]);
+  });
 
-  //     order_id = results.insertId;
-  //   });
+  [results] = await conn.query(sql, [values]);
 
-  //   sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`;
-
-  //   // items.. 배열 : 요소들을 하나씩 꺼내서 (foreach문 돌려서) >
-  //   values = [];
-  //   items.forEach((item) => {
-  //     values.push([order_id, item.book_id, item.quantity]);
-  //   });
-  //   conn.query(sql, [values], (err, results) => {
-  //     if (err) {
-  //       console.log(err);
-  //       return res.status(StatusCodes.BAD_REQUEST).end();
-  //     }
-
-  //     return res.status(StatusCodes.OK).json(results);
-  //   });
+  return res.status(StatusCodes.OK).json(results);
 };
 
 const getOrders = (req, res) => {
